d99f527f3d4ffed69b22257d77b971d555ecdbc4
caizhengjun <804213673@qq.com>
2020-06-24 14:54:34 +0800

    Java8

---
+++ spring-boot-java8/src/main/java/com/shanghai/springbootjava8/SpringBootJava8Application.java
@@ -0,0 +1,13 @@
+package com.shanghai.springbootjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJava8Application.class, args);
+    }
+
+}
